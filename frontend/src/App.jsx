01dfd2ab8cb3e8ed37b2f3347a0de56c5607498a
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2025-12-24 14:02:02 +0700

    Tạo dự án + tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <h3>Deck quan tam</h3>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
