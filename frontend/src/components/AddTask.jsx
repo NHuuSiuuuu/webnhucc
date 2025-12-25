d314d174e83f030b59c5a14ae95a092af45947a6
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2025-12-25 16:16:26 +0700

    Làm ô input

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -1,8 +1,18 @@
+import { Card } from "./ui/card";
+import { Input } from "./ui/input";
+
 function AddTask() {
   return (
-    <>
-      <h1>AddTask</h1>
-    </>
+    // Đây là component của ShadCn: Tạo ô hình chữ nhật
+    <Card className="p-6 border-0 bg-gradient-card shadow-custom-lg">
+      <div className="flex flex-col gap-3 sm:flex-row">
+        <Input
+          type="text"
+          placeholder="Cần phải làm gì?"
+          className="h-12 text-base border bg-slate-50 focus:border-primary/50 focus:ring-primary/20" // ring là lớp ngoài bao boder
+        />
+      </div>
+    </Card>
   );
 }
 
